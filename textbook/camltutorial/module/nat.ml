7d16596c16417ee0ac06135804b55c755693b143
Kohei Suenaga <ksuenaga@gmail.com>
2020-04-25 22:10:45 +0900

    Module

--- textbook/camltutorial/module/nat.ml
+++ textbook/camltutorial/module/nat.ml
@@ -2,15 +2,15 @@
 type nat = int
 
 let zero = 0
-let iszero n = n = 0
-let succ n = n + 1
+let iszero n = (n = 0)
+let succ n = (n + 1)
 let prev n =
     if n <= 0 then
         failwith "Not a natural number."
     else
         n - 1
 let repr n = n
-*)
+ *)
 
 type nat =
     | Zero
@@ -27,6 +27,6 @@ let prev n =
     | Zero -> failwith "Not a natural number."
     | Succ n' -> n'
 let rec repr n =
-  match n with
+  match n with 
   | Zero -> 0
   | Succ n' -> repr n' + 1
