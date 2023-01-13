50c1022ee6ab1b3f92c152a9d8f4176c9a1d2339
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2023-01-13 10:41:54 +0900

    format all files

--- textbook/interpreter/src/cui.ml
+++ textbook/interpreter/src/cui.ml
@@ -4,11 +4,14 @@ let rec read_eval_print env =
   print_string "# ";
   flush stdout;
   let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
-  let _ = print_string "parsing done\n"; flush stdout in
-  let (id, newenv, v) = eval_decl env decl in
+  let _ =
+    print_string "parsing done\n";
+    flush stdout
+  in
+  let id, newenv, v = eval_decl env decl in
   Printf.printf "val %s = " id;
   pp_val v;
-  print_newline();
+  print_newline ();
   read_eval_print newenv
 
 let initial_env =
