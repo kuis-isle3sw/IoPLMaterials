3d39d97e18de74658d57f4022b7ef773d63d308d
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2023-01-13 11:31:42 +0900

    solve Exercise 3.2.2

--- textbook/interpreter/src/cui.ml
+++ textbook/interpreter/src/cui.ml
@@ -3,16 +3,20 @@ open Eval
 let rec read_eval_print env =
   print_string "# ";
   flush stdout;
-  let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
-  let _ =
-    print_string "parsing done\n";
-    flush stdout
-  in
-  let id, newenv, v = eval_decl env decl in
-  Printf.printf "val %s = " id;
-  pp_val v;
-  print_newline ();
-  read_eval_print newenv
+  try
+    let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
+    let _ =
+      print_string "parsing done\n";
+      flush stdout
+    in
+    let id, newenv, v = eval_decl env decl in
+    Printf.printf "val %s = " id;
+    pp_val v;
+    print_newline ();
+    read_eval_print newenv
+  with exn ->
+    Printf.printf "Fatal error: exception %s\n" (Printexc.to_string exn);
+    read_eval_print env
 
 let initial_env =
   List.fold_left
