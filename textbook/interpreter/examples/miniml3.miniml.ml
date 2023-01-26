ec1c76bbabc8f175acc0083381adc0477449e980
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2023-01-26 22:03:44 +0900

    solve Exercise 3.4.6

--- textbook/interpreter/examples/miniml3.miniml.ml
+++ textbook/interpreter/examples/miniml3.miniml.ml
@@ -10,3 +10,28 @@ let makefact maker x = if x < 1 then 1 else x * maker maker (x + -1) in
 let fact x = makefact makefact x in
 fact 4
 ;;
+
+(* Exercise 3.4.6 *)
+(* 25 *)
+let fact = fun n -> n + 1 in
+let fact = fun n -> if n < 1 then 1 else n * fact (n + -1) in
+fact 5
+;;
+
+(* 25 *)
+let fact = dfun n -> n + 1 in
+let fact = fun n -> if n < 1 then 1 else n * fact (n + -1) in
+fact 5
+;;
+
+(* 120 *)
+let fact = fun n -> n + 1 in
+let fact = dfun n -> if n < 1 then 1 else n * fact (n + -1) in
+fact 5
+;;
+
+(* 120 *)
+let fact = dfun n -> n + 1 in
+let fact = dfun n -> if n < 1 then 1 else n * fact (n + -1) in
+fact 5
+;;
